518972eafd6c26a1bf44aa54f4e75c8d9f0b0401
mjohnston548 <53264004+mjohnston548@users.noreply.github.com>
2022-10-16 15:55:14 +0100

    Converted the app to use Shell for easier navigation. Added tabs and tab bar to switch between current and completed todos.

---
+++ TodoAppXamarin/TodoAppXamarin/Views/CompletedTodosPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TodoAppXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CompletedTodosPage : ContentPage
+    {
+        public CompletedTodosPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
